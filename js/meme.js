0b1938c332ab968a8106024122e6be8a39d7e630
Luiz F S Mariz <lfsmariz@gmail.com>
2020-09-07 09:54:46 -0300

    create function to insert text

--- js/meme.js
+++ js/meme.js
@@ -1,11 +1,15 @@
 const btnInputImage = document.querySelector('#meme-insert')
+const textInput = document.querySelector('#text-input')
 const imgMeme = document.querySelector('#imgmeme')
+const textMeme = document.querySelector("#text-content")
 
 btnInputImage.addEventListener('change', () => {
   imgMeme.src = window.URL.createObjectURL(btnInputImage.files[0])
-})
+});
 
-// imgMeme.src = btnInputImage.value
+textInput.addEventListener('input', () => {
+  textMeme.textContent = textInput.value
+});
 
-console.log(btnInputImage)
+
 
