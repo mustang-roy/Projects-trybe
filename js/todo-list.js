0b0d981ab44c1aff626b8137af0366a2a04c479e
Luiz F S Mariz <lfsmariz@gmail.com>
2020-09-10 10:43:13 -0300

    optmized createToDo with validation

--- js/todo-list.js
+++ js/todo-list.js
@@ -8,15 +8,51 @@ function selectItem(item) {
   item.classList.add('selected')
 }
 
+function completedItem(item) {
+  item.classList.remove('selected')
+  item.classList.toggle('completed')
+}
+
+function createToDo() {
+  if (textToDo.value != ''){
+    return addToDo()
+  }  
+    return alert('Input Vazio')
+}
+
 function addToDo() {
   let newItemList = document.createElement('li')
   newItemList.textContent = textToDo.value;
   newItemList.addEventListener('click', () => {selectItem(newItemList)})
+  newItemList.addEventListener('dblclick', () => {completedItem(newItemList)})
   listToDo.appendChild(newItemList)
+
+  textToDo.value = ''
 }
 
-const listToDo = document.querySelector('#list-todo')
+function removeAll (){
+  elementsToRemove = document.querySelectorAll('li')
+
+  for (const element of elementsToRemove) {
+    listToDo.removeChild(element)
+  }
+}
+
+function removeCompleted (){
+  elementsToRemove = document.querySelectorAll('.completed')
+
+  for (const element of elementsToRemove) {
+    listToDo.removeChild(element)
+  }
+}
+
+
+const listToDo = document.querySelector('#lista-tarefas')
 const textToDo = document.querySelector('#texto-tarefa');
 const btnAddToDo = document.querySelector('#criar-tarefa');
+const btnRemoveAll = document.querySelector('#apaga-tudo');
+const btnRemoveCompleted = document.querySelector('#remover-finalizados')
 
-btnAddToDo.addEventListener('click', addToDo)
+btnAddToDo.addEventListener('click', createToDo)
+btnRemoveAll.addEventListener('click', removeAll)
+btnRemoveCompleted.addEventListener('click', removeCompleted)
