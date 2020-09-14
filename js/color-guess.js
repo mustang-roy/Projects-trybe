517ecc60eed8d2d3fae25c1a12f221252abd4bc7
Luiz F S Mariz <lfsmariz@gmail.com>
2020-09-14 18:25:31 -0300

    function createRandomColor

---
+++ js/color-guess.js
@@ -0,0 +1,9 @@
+function createRandomColor(){
+  const red = Math.floor(Math.random()*256)
+  const green = Math.floor(Math.random()*256)
+  const blue = Math.floor(Math.random()*256)  
+
+  return `(${red}, ${green}, ${blue})`
+}
+
+const colorBoard = document.querySelector('#color-board')
